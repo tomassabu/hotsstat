14c406e851ae7ebb7c5c95a10350dfd5335fda52
tomassabu <tomasss89@gmail.com>
2017-10-21 14:53:06 +0200

    Updated the parser to read data from a player and then read data from each game file.

--- DataCollector.py
+++ DataCollector.py
@@ -1,14 +1,43 @@
-#curl -X GET "http://hotsapi.net/api/v1/replays?game_map=Volskaya%20Foundry&player=TommyGun%232872" -H "accept: application/json"
+#http://hotsapi.net/api/v1/replays?game_type=QuickMatch&player={playerName} ? {#battleTag}
+#http://hotsapi.net/api/v1/replays/{replay_id}
+
 import requests
+import json
 
 def callAPI():
-    print ("dette er hots-data")
 
-    url = "http://hotsapi.net/api/v1/replays?player=TommyGun%232872"
+#    url = "http://hotsapi.net/api/v1/replays?game_type=QuickMatch&player=TommyGun%232872"
+    url = "http://hotsapi.net/api/v1/replays?start_date=2017-10-01&game_type=QuickMatch&player=TommyGun%232872"
     resp = requests.get(url)
 
-    print (resp.status_code)
+    gameDict = resp.json()
+    antGames = len(gameDict)
+    print (antGames)
 
-    print (resp.content)
+    for gameNr in range(0,antGames):
+        game = gameDict[gameNr-1]
+        gameId = game["id"]
+        print (game["id"], gameNr)
+
+        getGameData(gameId)
+
+'''
+    url_game_id = "http://hotsapi.net/api/v1/replays/"+"gameId"
+
+    game_resp = requests.get(url_game_id)
+
+    print (game_resp.json().players[0])
+'''
+def getGameData(gameId):
+    url = "http://hotsapi.net/api/v1/replays/"+str(gameId)
+    print (url)
+    resp = requests.get(url)
+    gameDict = resp.json()
+    players = gameDict["players"]
+    for player in players:
+        if (player["battletag"] == "TommyGun#2872"):
+            print (player["battletag"], player["hero"], player["winner"],
+            player["score"]["hero_damage"], player["score"]["siege_damage"],
+            player["score"]["time_spent_dead"], player["score"]["experience_contribution"])
 
 callAPI()
--- README.md
+++ README.md
@@ -3,5 +3,19 @@
 Bruker HotsApi - https://hotsapi.net/docs
 
 #TODO
+-Hent data fra TommyGun
+-Hent data fra Tommy
+-Hent data fra Eowyn
+-Hent data fra Larsemann
+-Sjekk at det er riktig person ved å sjekke game replay og battletag
+-Lagre data
+  hero dmg
+  Siege dmg
+  hvilke heroes
+  seier og tap
+  time spent dead
+-Vis data på en fin måte
+-Sammenlign data
+
+
 -Søkefelt for søk etter spillers egne stats.
--Vis data på en fin måte.
