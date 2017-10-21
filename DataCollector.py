fe5ac2ea55748131985193ebf7d906f0cd6d3fee
tomassabu <tomasss89@gmail.com>
2017-10-21 17:13:31 +0200

    updated what data is collected and stores data in json-file

--- DataCollector.py
+++ DataCollector.py
@@ -1,5 +1,6 @@
 #http://hotsapi.net/api/v1/replays?game_type=QuickMatch&player={playerName} ? {#battleTag}
 #http://hotsapi.net/api/v1/replays/{replay_id}
+#saebu_no LhoeynJY saebu.no.mysql
 
 import requests
 import json
@@ -13,31 +14,42 @@ def callAPI():
     gameDict = resp.json()
     antGames = len(gameDict)
     print (antGames)
-
-    for gameNr in range(0,antGames):
+    data = {}
+    data['games'] = []
+#    f = open('TGhotsData.txt', 'w')
+    for gameNr in range(0,3):
         game = gameDict[gameNr-1]
         gameId = game["id"]
         print (game["id"], gameNr)
 
-        getGameData(gameId)
+        getGameData(gameId ,data)
+    with open('tg.json', 'w') as outfile:
+        json.dump(data, outfile)
 
-'''
-    url_game_id = "http://hotsapi.net/api/v1/replays/"+"gameId"
 
-    game_resp = requests.get(url_game_id)
-
-    print (game_resp.json().players[0])
-'''
-def getGameData(gameId):
+def getGameData(gameId ,data):
     url = "http://hotsapi.net/api/v1/replays/"+str(gameId)
+
     print (url)
     resp = requests.get(url)
     gameDict = resp.json()
     players = gameDict["players"]
+    gameDate = gameDict["game_date"]
     for player in players:
         if (player["battletag"] == "TommyGun#2872"):
-            print (player["battletag"], player["hero"], player["winner"],
-            player["score"]["hero_damage"], player["score"]["siege_damage"],
-            player["score"]["time_spent_dead"], player["score"]["experience_contribution"])
+            #playerData = (player["battletag"], player["hero"], player["winner"],
+            #player["score"]["hero_damage"], player["score"]["siege_damage"],
+            #player["score"]["time_spent_dead"], player["score"]["experience_contribution"],
+            #gameDate)
+            #f.write(str(playerData)+"\n")
+            data['games'].append({
+                'name' : player["battletag"],
+                'hero' : player["hero"],
+                'winner' : player["winner"],
+                'hero_dmg' : player["score"]["hero_damage"],
+                'siege_dmg' : player["score"]["siege_damage"],
+                'time_dead' : player["score"]["time_spent_dead"],
+                'exp_cont' : player["score"]["experience_contribution"],
+                'game_date' : gameDate})
 
 callAPI()
--- README.md
+++ README.md
@@ -1,21 +1,30 @@
 # hotsstat
 
 Bruker HotsApi - https://hotsapi.net/docs
+legg inn navn og battletag på spiller og generer en jsonfil med data om:
+  player,
+  hero,
+  winner,
+  hero dmg,
+  siege dmg,
+  exp contribution,
+  time spent dead,
+  data of game
 
 #TODO
--Hent data fra TommyGun
+-Hent data fra TommyGun X
 -Hent data fra Tommy
 -Hent data fra Eowyn
 -Hent data fra Larsemann
--Sjekk at det er riktig person ved å sjekke game replay og battletag
--Lagre data
+-Sjekk at det er riktig person ved å sjekke game replay og battletag X
+-Lagre data X
   hero dmg
   Siege dmg
   hvilke heroes
   seier og tap
   time spent dead
 -Vis data på en fin måte
--Sammenlign data
-
+-Lag en side som viser data i graf
+-lagre data i database i stedet for json-fil
 
 -Søkefelt for søk etter spillers egne stats.
